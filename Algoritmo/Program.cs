b16dfc29c02857816424793f988a79031ca62a9e
Casimiro <kaua.casimiro16@gmail.com>
2024-03-31 02:00:06 -0300

    Algoritmo par e impar

---
+++ Algoritmo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+
+            Console.WriteLine("Escreva um número");
+            n1 = double.Parse(Console.ReadLine());
+
+            if (n1 < 0 && n1 % 2 == 0)
+            {
+                Console.WriteLine("O numero é negativo");
+                Console.WriteLine("O numero é par");
+            } else if (n1 < 0 && n1 % 2 != 0)
+            {
+                Console.WriteLine("O numero é negativo");
+                Console.WriteLine("O numero é impar");
+            }
+            else if (n1 >= 0 && n1 % 2 == 0)
+            {
+                Console.WriteLine("O numero é positivo");
+                Console.WriteLine("O numero é par");
+            }
+            else
+            {
+                Console.WriteLine("O numero é positivo");
+                Console.WriteLine("O numero é impar");
+            }
+
+        }
+    }
+}
